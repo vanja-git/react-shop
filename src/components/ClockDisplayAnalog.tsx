79929d7923acc1023b254fa676a2d848b035dc86
Vanja Drobina <vanjadrobina@Vanjas-MacBook-Pro.local>
2022-11-25 17:56:52 +0100

    Analog clock glatke kazaljke uglovi

--- src/components/ClockDisplayAnalog.tsx
+++ src/components/ClockDisplayAnalog.tsx
@@ -3,9 +3,15 @@ const ClockDisplayAnalog = (props: any) => {
     const minute = props.minute;
     const sekunde = props.sekunde;
 
-    const ugaoSekunde = (360/60)*sekunde;
-    const ugaoMinute = (360/60)*minute;
-    const ugaoSati = (360/12)*sati;
+    const ugaoSekunde = (360/60)*sekunde; // sekunde grubi ugao
+
+    const ugaoMinute = (360/60)*minute; // minute grubi ugao
+    const minuteGlatke = minute + (sekunde/60);
+    const ugaoMinuteGlatke = (360/60)*minuteGlatke; //  minute glatki ugao
+
+    const ugaoSati = (360/12)*sati; // sati grubo ugao (0, 1, 2 ittd.)
+    const satiGlatki = sati + (minute / 60);
+    const ugaoSatiGlatki = (360/12)*satiGlatki; // sati glatki ugao (0, 0.01, 0.2, 0.5)
 
 
 
@@ -18,12 +24,20 @@ const ClockDisplayAnalog = (props: any) => {
                 }}>
                     <div className="kazaljka-sati"></div>
                 </div>
+                <div className="nosac-kazaljke" style={{
+                    transform: 'rotate(' + ugaoSatiGlatki + 'deg)'
+                }}>
+                    <div className="kazaljka-sati"></div>
+                </div>
+
 
                 <div className="nosac-kazaljke" style={{
-                    transform: 'rotate(' + ugaoMinute + 'deg)'
+                    transform: 'rotate(' + ugaoMinuteGlatke + 'deg)'
                 }}>
                     <div className="kazaljka-minute"></div>
                 </div>
+
+
 
                 <div className="nosac-kazaljke" style={{
                     transform: 'rotate(' + ugaoSekunde + 'deg)'
